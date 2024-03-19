15431e4593d14a8dd1f675348421ce04dbf9ba3d
aabdelmonaem <a.abdelmonaem@gmail.com>
2024-03-19 23:37:20 +0300

    update home page

--- template/src/components/pages/home/HomeView.jsx
+++ template/src/components/pages/home/HomeView.jsx
@@ -1,11 +1,44 @@
-import { useConfigContext, useLangContext } from "@aabdelmonaem/core-lib";
+import { useConfigContext, useLangContext, useNotification, useModalContext } from "@aabdelmonaem/core-lib";
 import styles from './home.module.css';
 
 
 const HomeView = () => {
-    const {getConfig} = useConfigContext();
-   
-    return <div className={styles.homeRoot}>home page Version {getConfig('version')}</div>;
+    const { getConfig } = useConfigContext();
+    const notification = useNotification();
+    const handleShow = (variant) => {
+        notification.show({
+            message: 'This is a sample Notification content displayed',
+            variant,
+        });
+    };
+    const [showModal, hideModal] = useModalContext(() => (
+        <div role="dialog" className="modal">
+            <p>This is a modal window</p>
+            <button onClick={hideModal}>Close</button>
+        </div>
+    ));
+    return (
+        <div className='App'>
+
+            <h3>useNotification hook</h3>
+            <div className='card'>
+                <button className='button' onClick={() => handleShow('error')}>
+                    Show Error
+                </button>
+                <button
+                    className='button'
+                    onClick={() => handleShow('success')}
+                >
+                    Show Success
+                </button>
+                <button className='button' onClick={() => handleShow('info')}>
+                    Show Info
+                </button>
+            </div>
+            <h3>useModalContext hook</h3>
+            <button onClick={showModal}>Show modal</button>
+        </div>
+    );
 };
 
 export default HomeView;
